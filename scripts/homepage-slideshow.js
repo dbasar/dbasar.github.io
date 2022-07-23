2890f4eb879c5c3c38b6980368f4b809b56d961d
Doruk Başar <dorukbasar@hotmail.com>
2022-07-23 13:44:48 +0300

    deg4

--- scripts/homepage-slideshow.js
+++ scripts/homepage-slideshow.js
@@ -1,35 +1,56 @@
 // ANASAYFADA GEÇİŞ YAPAN RESİMLER İÇİN
 
 // Asıl kutu content elemanı. Her bir resim, kutunun içinde absolute pozisyonunda yerleşik, yukarı kayan css animasyon hareketine sahip birer arkaplan resmine sahip küçük divler. Kayma işlemi için küçük div kutularının yüksekliği, content kutusundan biraz daha büyük. (Kutular orjinal pozisyonundayken kutuların tabanı content kutusunun tabanına değiyor, kayma sırasında yukarıda kalan ve görünmeyen kısım aşağıya iniyor.) Content kutusunda overflow: hidden; var. 
-// Her bir yeni kutunun animasyonu 7 (artık 8) sn sürüyor. Sayfa yüklendikten sonra her 6 sn de bir, kutu kopyalanıyor, kopya kutuya yeni arka resim ekleniyor, eski kutunun önüne geçmemesi için onun gerisine yerleştiriliyor. Eski kutunun opacitysi de 1 sn süren bir transition ile (css te her bir kutunun opacity transitionu var) 0 hale getiriliyor. 1 sn nin sonunda eski kutu yok ediliyor. 
+// Her bir yeni kutunun animasyonu 7 (artık 10) sn sürüyor. Sayfa yüklendikten sonra her 8 sn de bir, kutu kopyalanıyor, kopya kutuya yeni arka resim ekleniyor, eski kutunun önüne geçmemesi için onun gerisine yerleştiriliyor. Eski kutunun opacitysi de 2 sn süren bir transition ile (css te her bir kutunun opacity transitionu var) 0 hale getiriliyor. 2 sn nin sonunda eski kutu yok ediliyor. (8 sn boyunca kayması ve 2 sn de yok olması ile eski kutunun 10 sn lik animasyonu bitmiş oluyor.)
 
-const slideImages = ['../images/background-image.jpeg',
-                     '../images/furniture/furniture-11.jpeg',
-                     '../images/furniture/furniture-7.jpeg',
-                     '../images/industrial-design/design-2.jpeg'];
-let slideImagesCount = 0;
+const heroHeader = document.getElementById('hero-header');
+const heroLink = document.getElementById('hero-link');
+const slideImages = ['./images/background-image.jpeg',
+                     './images/furniture/furniture-11.jpeg',
+                     './images/industrial-design/design-2.jpeg',
+                     './images/furniture/furniture-7.jpeg'];
+let slideImageNum = 0;
 
-function change() {
+function imageChange() {
 
-    if (slideImagesCount === 3) {
-        slideImagesCount = 0;
+    heroLink.classList.add('fade');
+
+    if (slideImageNum === 3) {
+        slideImageNum = 0;
     } else {
-        slideImagesCount += 1;
+        slideImageNum += 1;
     }
     const imgNode = document.querySelector('.image-div');
     const replacerImgNode = imgNode.cloneNode();
 
-    replacerImgNode.style.backgroundImage = 'url(' + slideImages[slideImagesCount] + ')';
-    main.insertBefore(replacerImgNode, main.children[1]);
+    replacerImgNode.style.backgroundImage = 'url(' + slideImages[slideImageNum] + ')';
+    // main.insertBefore(replacerImgNode, main.children[1]);
+    heroHeader.insertBefore(replacerImgNode, heroHeader.firstElementChild);
     imgNode.style.opacity = "0";
 
     window.setTimeout(function () {
         imgNode.remove();
-    }, 1000);
+    }, 2000);
+
+    assignLink(slideImageNum);
+}
+
+function assignLink(slideImageNum) {
+    window.setTimeout(function() {
+        heroLink.classList.remove('fade');
+        if (slideImageNum === 0 || slideImageNum === 2) {
+            heroLink.firstElementChild.setAttribute('href', '/industrial-design.html');
+            heroLink.firstElementChild.firstElementChild.textContent = 'ÜRÜN';
+        } else {
+            heroLink.firstElementChild.setAttribute('href', '/furniture.html');
+            heroLink.firstElementChild.firstElementChild.textContent = 'MOBİLYA';
+        }    
+    }, 1000)
 }
 
 window.addEventListener('load', function() {
+    window.setInterval(imageChange, 8000);
     this.document.querySelector('.image-div').style.opacity = '1';
+    assignLink(0);
 });
 
-window.setInterval(change, 6000);
