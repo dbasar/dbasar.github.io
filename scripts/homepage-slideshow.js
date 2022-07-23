b914075cacc93d92e25c08113d79dc857c1a423e
Doruk Başar <dorukbasar@hotmail.com>
2022-07-24 00:00:41 +0300

    deg

--- scripts/homepage-slideshow.js
+++ scripts/homepage-slideshow.js
@@ -1,44 +1,46 @@
 // ANASAYFADA GEÇİŞ YAPAN RESİMLER İÇİN
 
+
 // Asıl kutu content elemanı. Her bir resim, kutunun içinde absolute pozisyonunda yerleşik, yukarı kayan css animasyon hareketine sahip birer arkaplan resmine sahip küçük divler. Kayma işlemi için küçük div kutularının yüksekliği, content kutusundan biraz daha büyük. (Kutular orjinal pozisyonundayken kutuların tabanı content kutusunun tabanına değiyor, kayma sırasında yukarıda kalan ve görünmeyen kısım aşağıya iniyor.) Content kutusunda overflow: hidden; var. 
-// Her bir yeni kutunun animasyonu 7 (artık 10) sn sürüyor. Sayfa yüklendikten sonra her 8 sn de bir, kutu kopyalanıyor, kopya kutuya yeni arka resim ekleniyor, eski kutunun önüne geçmemesi için onun gerisine yerleştiriliyor. Eski kutunun opacitysi de 2 sn süren bir transition ile (css te her bir kutunun opacity transitionu var) 0 hale getiriliyor. 2 sn nin sonunda eski kutu yok ediliyor. (8 sn boyunca kayması ve 2 sn de yok olması ile eski kutunun 10 sn lik animasyonu bitmiş oluyor.)
+
+// Her bir yeni kutunun animasyonu 10 sn sürüyor. Sayfa yüklendikten sonra her 8 sn de bir, mevcut active class lı kutuyua ending classı ekleniyor (böylece z-index i düşürülüyor), bir sonraki kutuya active classı ekleniyor (z-index i daha büyük). Yeni active class lı kutunun opacity si yavaşça 0 dan 1 e 2 sn de çıkıyor. Eski .active.ending kutunun animasyonu devam ediyor (animasyon .active de tanımlı). 2 sn nin sonunda eski kutudan active ve ending classları siliniyor ve onun standart, animasyon yapmayan en küçük z indeksli ve opacity si 0 olan diğer kutulardan olması sağlanıyor. bu işlem 8 + 2 = 10 sn de tamamlanıyor. 
+
 
 const heroHeader = document.getElementById('hero-header');
 const heroLink = document.getElementById('hero-link');
-const slideImages = ['./images/background-image.jpeg',
-                     './images/furniture/furniture-11.jpeg',
-                     './images/industrial-design/design-2.jpeg',
-                     './images/furniture/furniture-7.jpeg'];
-let slideImageNum = 0;
+const imgNodes = document.querySelectorAll('.image-div');
+
+let newImgIndex, oldImgIndex;
+
 
 function imageChange() {
-
     heroLink.classList.add('fade');
 
-    if (slideImageNum === 3) {
-        slideImageNum = 0;
+    oldImgIndex = Array.from(imgNodes)
+                       .indexOf(document.querySelector('.image-div.active'));
+
+    imgNodes[oldImgIndex].classList.add('ending');
+
+    if (oldImgIndex === 3) {
+        newImgIndex = 0;
     } else {
-        slideImageNum += 1;
+        newImgIndex = oldImgIndex + 1;
     }
-    const imgNode = document.querySelector('.image-div');
-    const replacerImgNode = imgNode.cloneNode();
+    imgNodes[newImgIndex].classList.add('active');
+    
+    window.setTimeout(function() {
+        imgNodes[oldImgIndex].classList.remove('ending', 'active');
+    }, 2000);    
 
-    replacerImgNode.style.backgroundImage = 'url(' + slideImages[slideImageNum] + ')';
-    // main.insertBefore(replacerImgNode, main.children[1]);
-    heroHeader.insertBefore(replacerImgNode, heroHeader.firstElementChild);
-    imgNode.style.opacity = "0";
-
-    window.setTimeout(function () {
-        imgNode.remove();
-    }, 2000);
-
-    assignLink(slideImageNum);
+    assignLink(newImgIndex);
 }
 
-function assignLink(slideImageNum) {
+function assignLink(imgIndex) {
     window.setTimeout(function() {
+
         heroLink.classList.remove('fade');
-        if (slideImageNum === 0 || slideImageNum === 2) {
+
+        if (imgIndex === 0 || imgIndex === 2) {
             heroLink.firstElementChild.setAttribute('href', '/industrial-design.html');
             heroLink.firstElementChild.firstElementChild.textContent = 'ÜRÜN';
         } else {
@@ -48,9 +50,9 @@ function assignLink(slideImageNum) {
     }, 1000)
 }
 
+
 window.addEventListener('load', function() {
     window.setInterval(imageChange, 8000);
-    this.document.querySelector('.image-div').style.opacity = '1';
     assignLink(0);
 });
 
